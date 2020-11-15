3942ef954529a94596f4b1f7a44e958032bb975b
Alexander Profeit <alex@profeit.com>
2020-11-15 03:11:41 -0500

    update history url for production

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 import fs from 'fs'
 
-const HISTORY_URL = process.env.PORT || 'http://localhost:3000/messages'
+const HISTORY_URL = process.env.HISTORY_URL || 'http://localhost:3000/messages'
 const ALLOWED_THREAD_IDS = ['4026672720681638', '100057382012154']
 
 login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, { forceLogin: true, selfListen: true }, (err, api) => {
