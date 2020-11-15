501b4c9336a82f9064d232aaf18257630c7ab2a6
Alexander Profeit <alex@profeit.com>
2020-11-15 00:31:58 -0500

    cleanup

--- index.js
+++ index.js
@@ -4,20 +4,15 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 
 const HISTORY_URL = 'http://mockbin.org/bin/8a2b1956-215f-4f83-8e2f-57bf0ff25807'
+const THREAD_ID = '4026672720681638'
 
 login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, { forceLogin: true, selfListen: true }, (err, api) => {
   if (err) return console.error(err)
 
-  api.getFriendsList((err, data) => {
-    if (err) return console.error(err)
-
-    fs.writeFileSync('friendslist.json', JSON.stringify(api.getAppState()))
-  })
-
   api.listenMqtt((err, message) => {
     if (err) return console.error(err)
 
-    if (message.type !== 'message') {
+    if (message.type !== 'message' || message.threadID !== THREAD_ID) {
       return
     }
 
@@ -26,7 +21,7 @@ login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, { forceL
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(message.body)
+      body: JSON.stringify({ message: message })
     })
 
     console.log(message)
