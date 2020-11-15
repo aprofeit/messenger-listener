a0dc6dcc90f60735cbe3b88c45e028b5f190d8e4
Alexander Profeit <alex@profeit.com>
2020-11-15 03:30:52 -0500

    remove app state

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 
 const HISTORY_URL = process.env.HISTORY_URL || 'http://localhost:3000/messages'
-const ALLOWED_THREAD_IDS = ['4026672720681638', '100057382012154']
+const ALLOWED_THREAD_IDS = ['4026672720681638', '100057382012154', '503003510']
 
 login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, { forceLogin: true, selfListen: true, autoMarkRead: true }, (err, api) => {
   if (err) return console.error(err)
@@ -13,6 +13,7 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, { forc
   api.listenMqtt((err, message) => {
     if (err) return console.error(err)
 
+    console.log(message.threadID)
     if (!ALLOWED_THREAD_IDS.includes(message.threadID)) {
       return
     }
