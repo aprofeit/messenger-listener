88242e977b2e5374608755d5e5c659949c5d7842
Alexander Profeit <alex@profeit.com>
2020-11-15 02:46:20 -0500

    force login

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ import fs from 'fs'
 const USER = process.env.USER
 const PASS = process.env.PASS
 
-login({ email: USER, password: PASS }, (err, api) => {
+login({ email: USER, password: PASS }, { forceLogin: true }, (err, api) => {
   if (err) return console.error(err)
 
   fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()))
